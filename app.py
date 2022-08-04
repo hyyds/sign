32f52c05d4a0bf0a02a0ea780616dc7907b7baf1
hyyds <85011938+hyyds@users.noreply.github.com>
2022-08-04 23:28:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,13 @@ def main():
     body = request.values.get('body') or request.get_json()['body']
     wskey = request.values.get('wskey')
     if fn:
-        sign = get_sign(fn, body, "".join(str(uuid.uuid4()).split("-")), "android", "11.1.4")
+        while True:
+            try:
+                sign = get_sign(fn, body, "".join(str(uuid.uuid4()).split("-")), "android", "11.1.4")
+            Exception as e:
+                pass
+            finally:
+                break          
         res = {"code": 200, "data": {"sign": f'body={quote(body)}&{sign}'}}
     else:
         res = {"code": 400, "data": "请传入url参数！"}
